75cf19f494dcabae052398c4a08b7bec10b3fb66
Alexis <alexis.prunier@securitymadein.lu>
2023-11-11 15:24:48 +0100

    Be eslint compliant

--- src/pages/PageBuilder.js
+++ src/pages/PageBuilder.js
@@ -3,10 +3,10 @@ import "./PageBuilder.css";
 
 interface PageBuilderProps {}
 
-const PageBuilder: React.FC<PageBuilderProps> = ({}) => {
+const PageBuilder: React.FC<PageBuilderProps> = () => {
   return (
     <div id="PageBuilder" className="d-flex align-items-center justify-content-center w-100 h-100">
-      <img src="/media/images/mower.svg" className="mower"/>
+      <img src="/media/images/mower.svg" className="mower" alt="Mower"/>
       <span>Gardeners are working on it...</span>
     </div>
   );
