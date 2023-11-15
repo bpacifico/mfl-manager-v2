d277196ae426fe80c18a1c0da406cf5ed430d599
Alexis <alexis.prunier@securitymadein.lu>
2023-11-15 16:20:15 +0100

    Add loading for 2 pages

--- src/pages/pagestats/PageStatsClubs.js
+++ src/pages/pagestats/PageStatsClubs.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import "./PageStatsClubs.css";
 
 interface PageStatsClubsProps {}
 
 const PageStatsClubs: React.FC<PageStatsClubsProps> = ({ initialValue }) => {
   return (
-    <div className="row">
-      <div className="col">
-      </div>
+    <div id="PageStatsClubs" className="d-flex align-items-center justify-content-center w-100 h-100">
+      <img src="/media/images/mower.svg" className="mower" alt="Mower"/>
+      <span>Gardeners are working on it...</span>
     </div>
   );
 };
--- src/pages/pagestats/PageStatsPlayers.js
+++ src/pages/pagestats/PageStatsPlayers.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import "./PageStatsPlayers.css";
 
 interface PageStatsPlayersProps {}
 
 const PageStatsPlayers: React.FC<PageStatsPlayersProps> = ({ initialValue }) => {
   return (
-    <div className="row">
-      <div className="col">
-      </div>
+    <div id="PageStatsPlayers" className="d-flex align-items-center justify-content-center w-100 h-100">
+      <img src="/media/images/mower.svg" className="mower" alt="Mower"/>
+      <span>Gardeners are working on it...</span>
     </div>
   );
 };
