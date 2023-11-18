cf76cd585a45450135fc7ae4c4fb54aea158acd2
Alexis Prunier <prunier.al@gmail.com>
2023-11-18 14:12:09 +0100

    Review landing page

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -8,17 +8,17 @@ const PageHome: React.FC<PageHomeProps> = () => {
     <div id="PageHome" className="container py-4">
       <div className="row">
         <div className="offset-xl-2 offset-lg-1 col-xl-8 col-lg-10">
-          <div className="row my-5">
-            <div className="col-md-6">
+          <div className="row min-vh-100">
+            <div className="col-md-6 d-flex align-items-end align-items-md-center pe-5 ps-5 p-md-0">
                 <img
-                  className="w-100 pri"
+                  className="w-100 mb-0 mb-md-5"
                   src="/media/images/assistant.svg"
                   alt="MFL Assistant"
                 />
             </div>
 
-            <div className="col-md-6 py-4 d-flex align-items-center">
-              <div className="flex-column">
+            <div className="col-md-6 py-4 d-flex align-items-start align-items-md-center text-center text-md-start">
+              <div className="flex-column w-100 w-md-auto">
                 <span>Tighten your shoelaces thanks to the</span>
                 <h1 className="text-white">MFL Assistant.</h1>
               </div>
@@ -30,7 +30,7 @@ const PageHome: React.FC<PageHomeProps> = () => {
               <h1 className="text-white text-end">Get an overview of the MFL activity</h1>
             </div>
 
-            <div className="col-md-4">
+            <div className="col-md-4 mb-2">
               <Link to="/stats/competitions" className="text-decoration-none">
                 <div className="card h-100">
                   <img src="/media/images/podium.svg" className="card-img-top" alt="Random"></img>
@@ -42,7 +42,7 @@ const PageHome: React.FC<PageHomeProps> = () => {
               </Link>
             </div>
 
-            <div className="col-md-4">
+            <div className="col-md-4 mb-2">
               <Link to="/stats/clubs" className="text-decoration-none">
                 <div className="card h-100">
                   <img src="/media/images/buildings.svg" className="card-img-top p-4" alt="Random"></img>
@@ -54,7 +54,7 @@ const PageHome: React.FC<PageHomeProps> = () => {
               </Link>
             </div>
 
-            <div className="col-md-4">
+            <div className="col-md-4 mb-2">
               <Link to="/stats/players" className="text-decoration-none">
                 <div className="card h-100">
                   <img src="/media/images/player.svg" className="card-img-top p-4" alt="Random"></img>
@@ -72,7 +72,7 @@ const PageHome: React.FC<PageHomeProps> = () => {
               <h1 className="text-white">Directors, time to step up your clubs!</h1>
             </div>
 
-            <div className="offset-md-1 col-md-5">
+            <div className="offset-md-1 col-md-5 mb-2">
               <Link to="/mercato/contracts" className="text-decoration-none">
                 <div className="card h-100">
                   <img src="/media/images/contract-search.svg" className="card-img-top" alt="Random"></img>
@@ -84,7 +84,7 @@ const PageHome: React.FC<PageHomeProps> = () => {
               </Link>
             </div>
 
-            <div className="col-md-5">
+            <div className="col-md-5 mb-2">
               <Link to="/mercato/trades" className="text-decoration-none">
                 <div className="card h-100">
                   <img src="/media/images/scout.svg" className="card-img-top p-4" alt="Random"></img>
@@ -102,7 +102,7 @@ const PageHome: React.FC<PageHomeProps> = () => {
               <h1 className="text-white text-end">You have a business to grow, Agents!</h1>
             </div>
 
-            <div className="offset-md-1 col-md-5">
+            <div className="offset-md-1 col-md-5 mb-2">
               <Link to="/mercato/contracts" className="text-decoration-none">
                 <div className="card h-100">
                   <img src="/media/images/increase.svg" className="card-img-top p-4" alt="Random"></img>
@@ -114,7 +114,7 @@ const PageHome: React.FC<PageHomeProps> = () => {
               </Link>
             </div>
 
-            <div className="col-md-5">
+            <div className="col-md-5 mb-2">
               <Link to="/mercato/trades" className="text-decoration-none">
                 <div className="card h-100">
                   <img src="/media/images/exchange.svg" className="card-img-top p-4" alt="Random"></img>
