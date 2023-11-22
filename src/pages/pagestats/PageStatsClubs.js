62d4e012bad93aeba8dae17e9ec50b21c6133cb1
Alexis <alexis.prunier@securitymadein.lu>
2023-11-22 13:57:28 +0100

    Unfinished progress on the stats/clubs page

--- src/pages/pagestats/PageStatsClubs.js
+++ src/pages/pagestats/PageStatsClubs.js
@@ -1,29 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Count from "components/counts/Count.js";
 import ChartPyramidClubDivisions from "components/charts/ChartPyramidClubDivisions.js";
 
 interface PageStatsClubsProps {}
 
-const PageStatsClubs: React.FC<PageStatsClubsProps> = ({ initialValue }) => {
+const PageStatsClubs: React.FC<PageStatsClubsProps> = () => {
+  const [clubs, setClubs] = useState(null);
+
+  useEffect(() => {
+    fetch("/data/mfl_clubs.json")
+     .then((response) => response.json())
+     .then((responseJson) => {
+       setClubs(responseJson);
+     })
+     .catch((error) => {
+       console.error(error);
+     });
+  }, []);
+
   return (
     <div className="row">
       <div className="col">
         <div className="row mt-md-2 mb-md-5">
-          <div className="col-12">
-            <div class="alert alert-info" role="alert">
-              The displayed data is for demonstration purposes only
+          <div className="col-12 mb-3">
+            <div className="alert alert-info" role="alert">
+              The input data is a snapshot prior to the issuance of the stone clubs 
             </div>
           </div>
           <div className="offset-lg-2 col-lg-2 col-sm-4">
             <Count
               label="Clubs"
-              count={"XX"}
+              count={clubs?.length}
             />
           </div>
           <div className="offset-lg-1 col-lg-2 col-sm-4">
             <Count
-              label="Club owners"
-              count={"XX"}
+              label="Countries"
+              count={clubs
+                ? [...new Set(clubs.map((c) => c.country_code))].length
+                : undefined
+              }
             />
           </div>
           <div className="offset-lg-1 col-lg-2 col-sm-4">
@@ -35,6 +51,22 @@ const PageStatsClubs: React.FC<PageStatsClubsProps> = ({ initialValue }) => {
         </div>
 
         <div className="row">
+          <div className="col-sm-6">
+            <h4 className="pb-3">Division distribution</h4>
+
+            <ChartPyramidClubDivisions
+              clubs={[]}
+            />
+          </div>
+
+          <div className="col-sm-6">
+            <h4 className="pb-3">Division distribution</h4>
+
+            <ChartPyramidClubDivisions
+              clubs={[]}
+            />
+          </div>
+
           <div className="col-sm-12">
             <h4 className="pb-3">Division distribution</h4>
 
