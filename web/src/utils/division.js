74cdb43d92ac3a7389fdf276c3698d48c2fbd57d
Alexis Prunier <alexis.prunier@level.eu>
2024-06-14 16:54:12 +0200

    update division

--- web/src/utils/division.js
+++ web/src/utils/division.js
@@ -1,12 +1,12 @@
-
 export const divisions = [
-  { number: 1, name: "Diamond",   color: "rgb(59, 233, 248)" },
-  { number: 2, name: "Platinum",  color: "rgb(19, 211, 137)" },
-  { number: 3, name: "Gold",      color: "rgb(255, 210, 62)" },
-  { number: 4, name: "Silver",    color: "rgb(243, 243, 243)" },
-  { number: 5, name: "Bronze",    color: "rgb(253, 122, 0)" },
-  { number: 6, name: "Iron",      color: "rgb(134, 94, 63)" },
-  { number: 7, name: "Stone",     color: "rgb(183, 176, 156)" },
+  { number: 1, name: "Diamond", color: "rgb(59, 233, 248)" },
+  { number: 2, name: "Platinum", color: "rgb(19, 211, 137)" },
+  { number: 3, name: "Gold", color: "rgb(255, 210, 62)" },
+  { number: 4, name: "Silver", color: "rgb(243, 243, 243)" },
+  { number: 5, name: "Bronze", color: "rgb(253, 122, 0)" },
+  { number: 6, name: "Iron", color: "rgb(134, 94, 63)" },
+  { number: 7, name: "Stone", color: "rgb(183, 176, 156)" },
+  { number: 8, name: "Ice", color: "rgb(183, 176, 156)" },
 ];
 
 export const getDivisionColor = (division) => {
