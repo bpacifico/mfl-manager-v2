2061ba8d5583c58eccc0a52eb0f6736c9e8827fe
Alexis Prunier <alexis.prunier@level.eu>
2024-06-15 11:50:45 +0200

    Set Ice color

--- web/src/utils/division.js
+++ web/src/utils/division.js
@@ -6,7 +6,7 @@ export const divisions = [
   { number: 5, name: "Bronze", color: "rgb(253, 122, 0)" },
   { number: 6, name: "Iron", color: "rgb(134, 94, 63)" },
   { number: 7, name: "Stone", color: "rgb(183, 176, 156)" },
-  { number: 8, name: "Ice", color: "rgb(183, 176, 156)" },
+  { number: 8, name: "Ice", color: "rgb(176, 204, 225)" },
 ];
 
 export const getDivisionColor = (division) => {
