a0789509ea966dcc70caec579f67b2b72f4260ed
Alexis Prunier <alexis.prunier@level.eu>
2024-07-27 23:12:41 +0200

    Fix display on popup

--- web/src/components/popups/PopupAddPlayers.js
+++ web/src/components/popups/PopupAddPlayers.js
@@ -134,7 +134,7 @@ const PopupAddPlayers: React.FC < PopupAddPlayersProps > = ({ trigger, onClose, 
         className={"fade-in popup-xl"}
       >
         {(close) => (
-          <div className="container bg-dark d-flex flex-column border border-info border-3 rounded-3 vh-95 p-4">
+          <div className="container bg-dark d-flex flex-column border border-info border-3 rounded-3 p-4">
             <div className="d-flex flex-row flex-grow-0 mb-3">
               <div className="flex-grow-1">
                 <h2 className="text-white">
